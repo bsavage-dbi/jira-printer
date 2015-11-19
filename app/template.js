679154dc6f86ac141f52f564abec67ff7e8ffb04
Federico Marzocchi <hello@mrz.io>
2015-11-19 17:20:39 +0100

    reformat code

--- app/template.js
+++ app/template.js
@@ -1,24 +1,29 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if  (request.action == 'fill') {
-      console.log(request);
-      window.storiesData = request.stories;
-      fillStories(request.stories)
+    function (request, sender, sendResponse) {
+        if (request.action == 'fill') {
+            console.log(request);
+            window.storiesData = request.stories;
+            fillStories(request.stories)
+        }
     }
-  }
 );
 
-function fillStories(data) 
-{
-  chrome.storage.sync.get('storyTemplate', function(storedData) {
-    var template = storedData.storyTemplate.length > 5 ? storedData.storyTemplate : $("#template").html();
-    $("#container").html("");
-    for(i in data) {
-        $("#container").append(Mustache.render(template, data[i]))
-    }
-  });
+function fillStories(data) {
+    chrome.storage.sync.get('storyTemplate', function (storedData) {
+        var template = storedData.storyTemplate.length > 5 ? storedData.storyTemplate : $("#template").html();
+        $("#container").html("");
+        for (i in data) {
+            $("#container").append(Mustache.render(template, data[i]))
+        }
+    });
 }
 
-$(function(){
-  fillStories([{"key":"...","type":"...","summary":"Loading stories...","img":"https://jira.rocket-internet.de/images/icons/issuetypes/documentation.png","estimation":"..."}])
+$(function () {
+    fillStories([{
+        "key": "...",
+        "type": "...",
+        "summary": "Loading stories...",
+        "img": "https://jira.rocket-internet.de/images/icons/issuetypes/documentation.png",
+        "estimation": "..."
+    }])
 })
